bc72eba809f85b58f083c56be7cac07ae0efc412
joneseyit <joneseyit@protonmail.com>
2018-10-19 16:13:06 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  puts "Welcome #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def line(katz_deli)
