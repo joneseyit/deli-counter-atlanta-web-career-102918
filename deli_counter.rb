f3b5b6c7c83dbc02e83a067818c77fa0a78f4166
joneseyit <joneseyit@protonmail.com>
2018-10-19 16:22:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0 
-    puts "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!"
   end
   puts "Currently serving #{katz_deli.shift}."
 end
