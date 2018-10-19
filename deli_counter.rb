e2750ca0b3d2af2545a9de835e47d71d1e2ff2c0
joneseyit <joneseyit@protonmail.com>
2018-10-19 16:01:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli = []
+def line(katz_deli)
+  katz_deli.each_with_index do |name, idx|
+    puts "Welcome, #{name}. You are number #{idx + 1} in line. "
+end
+
+def take_a_number(katz_deli)
+  puts "Welcome #{name}.  You are number #{katz_deli.length} in line."
+end
+
+def line(katz_deli)
+  new_string = "The line is currently:"
+  katz_deli.each_with_index do |name, idx|
+    new_string += " #{idx + 1}. #{name}"
+  end
+end
+
+def now_serving(katz_deli)
+  
+end
