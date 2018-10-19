513cd76995972fabc5131900be2879d8465ab91e
joneseyit <joneseyit@protonmail.com>
2018-10-19 16:11:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli)
-  puts "The line is currently empty." if katz_deli.length == 0 
-  katz_deli.each_with_index do |name, idx|
-    puts "Welcome, #{name}. You are number #{idx + 1} in line. "
-  end
-end
+# def line(katz_deli)
+#   puts "The line is currently empty." if katz_deli.length == 0 
+#   katz_deli.each_with_index do |name, idx|
+#     puts "Welcome, #{name}. You are number #{idx + 1} in line. "
+#   end
+# end
 
 def take_a_number(katz_deli, name)
   katz_deli << name
