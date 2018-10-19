75eaf152f03c1d2748070d13ae6be22076073919
joneseyit <joneseyit@protonmail.com>
2018-10-19 16:04:55 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 # Write your code here.
 katz_deli = []
 def line(katz_deli)
+  puts "The line is currently empty." if katz_deli.length == 0 
   katz_deli.each_with_index do |name, idx|
     puts "Welcome, #{name}. You are number #{idx + 1} in line. "
   end
